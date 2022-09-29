a7d0037b0ea3a55b11c3ea6efce66ff92637f123
maobri-s <pamir-natasha@mail.ru>
2022-09-29 12:05:09 +0700

    Прошёл второй урок

---
+++ lesson_1/main.py
@@ -0,0 +1,36 @@
+# # print("Hello world")
+# # message_1 = input("Сообщение 1:")
+# #
+# # print(type(message_1))
+# name = input("Твоё имя: ")
+# age = input("Твой возраст: ")
+# time = input("Текущее время: ")
+#
+# print(""Меня зовут" {name}, "мне" {age} "лет." "И сейчас" {time}!")
+# a = int(input("a:"))
+# b = int(input("b:"))
+#
+# p = (a + b) * 2
+# s = a * b
+#
+# print("P=", p)
+# print("S=", s)
+
+# a = input("a: ")
+# b = input("b: ")
+#
+#
+# c = a
+# a = b
+# b = c
+# print("a =", a)
+# print("b =", b)
+# int(input("a: "))
+# int(input("b: "))
+#
+# print((a+b) / 2)
+hours = int(input("h: "))
+minutes = int(input("m:"))
+seconds = int(input("s: "))
+
+print(hours * 60 * 60 + minutes * 60 + seconds)
